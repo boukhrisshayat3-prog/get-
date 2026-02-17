4841c8bb91d4fea4de73faeb74f6044fbcd41908
Full Stack <fullstack@Valencia-Aula-9.local>
2026-02-17 20:24:21 +0100

    first commit

---
+++ README.ma
@@ -0,0 +1,22 @@
+# Actividad Warm Up
+
+## Objetivo
+Crear 02 bases de datos nuevas con ejercicios de conexión.
+
+## Ejercicios
+
+### 1era Base de Datos
+- Ejercicio de conexión con ping
+
+### 2da Base de Datos
+- Conexión y GET en los paths que ustedes consideren
+
+## Requisitos
+
+- Ser subido a un proyecto en GitHub
+- **No subir el .env**
+- Tiempo estimado: 30-40 minutos
+- Crear 02 repositorios (uno por cada ejercicio)
+- Trabajar en una rama separada de main llamada `feature`
+- Tener un archivo inicial en main (por ejemplo: txt)
+azn-ztyg-yvk
---
+++ index.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+    serverApi:{
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+
+app.get('/Valence', async (req, res)=> {
+    try {
+      await client.connect();
+      const db = client.db("Hotels");
+      const Valence = db.collection("Valence")
+      const lista = await Valence.find({}).toArray();
+      res.json({success: true, data: lista})  
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message})
+    }finally{
+        await client.close();
+    }
+})
+
+app.listen(PORT, () =>{
+    console.log(`your port is http://localhost:${PORT}`)
+})
